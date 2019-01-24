216105f24faa41904a91c92118e3c09d86761cd5
Wangxy <93236565@qq.com>
2019-01-25 01:21:13 +0800

    add request collection.

--- src/main/scala/com/github/apuex/accesslog/analyser/Main.scala
+++ src/main/scala/com/github/apuex/accesslog/analyser/Main.scala
@@ -1,7 +1,9 @@
 package com.github.apuex.accesslog.analyser
 
 import java.io._
+import java.util.concurrent.TimeUnit
 import java.util.regex._
+
 import com.github.apuex.accesslog.ApacheHttpdParser._
 import com.github.apuex.accesslog.Field._
 import com.github.apuex.springbootsolution.runtime._
@@ -14,6 +16,8 @@ import akka.actor.ActorSystem
 import akka.event._
 
 import scala.concurrent._
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Success
 
 object Main extends App {
   implicit val system = ActorSystem("system")
@@ -22,34 +26,37 @@ object Main extends App {
 
 
   if(args.length == 1) {
-    analyse().map(s => {
-      s.toSet[String].foreach(x => println(x))
-      materializer.shutdown()
-      system.terminate()
-    })
+    val (s1, s2) = analyse()
+    val futures = Seq(s1, s2)
+    futures.foreach(s => s.map(s => s.toSet[String].foreach(x => println(x))))
+    futures.foreach(f => Await.ready(f, FiniteDuration(1000, TimeUnit.DAYS)))
+    materializer.shutdown()
+    system.terminate()
   } else {
     println("usage: <cmd> <log dir>")
+    materializer.shutdown()
+    system.terminate()
   }
 
-  def analyse(): Future[Seq[String]] = {
+  def analyse(): (Future[Seq[String]], Future[Seq[String]]) = {
+    val requestSink = Sink.seq[String]
     val userAgentSink = Sink.seq[String]
-    RunnableGraph.fromGraph(GraphDSL.create(userAgentSink) { implicit builder =>
-      (theUserAgentSink) =>
+    RunnableGraph.fromGraph(GraphDSL.create(requestSink, userAgentSink)((_, _)) { implicit builder =>
+      (theRequestSink, theUserAgentSink) =>
       import akka.stream.scaladsl.GraphDSL.Implicits._
 
       val logs = accessLog(new File(args(0)))
       val tokenizer = Flow[String].map(x => tokenLine(x, combinedPattern).filter(_ != null)).filter(!_.isEmpty)
-      //val broadcast = builder.add(Broadcast[Array[String]](1))
+      val broadcast = builder.add(Broadcast[Array[String]](2))
       val userAgentColIndex = combined.indexOf(UserAgent)
       val requestColIndex = combined.indexOf(Request)
-      //val request = Flow[Array[String]].map(x => x(requestColIndex))
+      val request = Flow[Array[String]].map(x => x(requestColIndex))
       val userAgent = Flow[Array[String]].map(x => x(userAgentColIndex))
           .log("error")
 
-      logs ~> tokenizer ~> userAgent ~> theUserAgentSink.in
-       // Sink.foreach[String](x => println(x))
+      logs ~> tokenizer ~> broadcast ~> userAgent ~> theUserAgentSink.in
 
-      //broadcast ~> request ~> Sink.foreach[String](x => println(x))
+      broadcast ~> request ~> theRequestSink.in
 
       ClosedShape
     }).run()(materializer)
